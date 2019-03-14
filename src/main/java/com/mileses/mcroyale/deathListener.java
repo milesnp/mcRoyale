9c4f8950cb40fa4f4b917f0ee93ca6edeeccffb9
milesnp <milesnp+github@gmail.com>
2019-03-13 21:53:45 -0700

    Update deathListener.java

--- src/main/java/com/mileses/mcroyale/deathListener.java
+++ src/main/java/com/mileses/mcroyale/deathListener.java
@@ -1,6 +1,7 @@
 package com.mileses.mcroyale;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -11,13 +12,17 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public final class deathListener implements Listener{
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent e) {
+		//harvest info 
 		Bukkit.getLogger().info("death occurs");
 		Player player = e.getEntity();
-		if (player.getKiller() instanceof Player) {
+		Location location = player.getLocation();
+		location.setY(160);
+		//TEST TODO UNDO TEST PROBLEM
+		if (!(player.getKiller() instanceof Player)) {
 			Bukkit.getLogger().info("death by player");
 			Player killer = player.getKiller();
 			World world = killer.getWorld();
-			world.playSound(killer.getLocation(), Sound.EXPLODE, 200, 1);
+			world.playSound(location, Sound.EXPLODE, 220, 1);
 			Bukkit.broadcastMessage(killer.getName()+" killed "+player.getName());
 		}
 		else Bukkit.getLogger().info("not by player");
