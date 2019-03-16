2f4e682a43481b72f3e1524e4fee8fd5529dfb68
milesnp <milesnp+github@gmail.com>
2019-03-15 17:32:18 -0700

    Update McRoyaleDeathListener.java

--- src/main/java/com/mileses/mcroyale/McRoyaleDeathListener.java
+++ src/main/java/com/mileses/mcroyale/McRoyaleDeathListener.java
@@ -2,13 +2,11 @@ package com.mileses.mcroyale;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.scheduler.BukkitScheduler;
 import de.myzelyam.api.vanish.VanishAPI;
 
 public final class McRoyaleDeathListener implements Listener{
@@ -23,15 +21,16 @@ public final class McRoyaleDeathListener implements Listener{
 		 final World world = player.getWorld();
 		//check if killed by a player
 		if ((player.getKiller() instanceof Player)) {
-			Bukkit.getLogger().info("death by player");
 			Player killer = player.getKiller();
+			Bukkit.getLogger().info(player.getName()+" died by player " + killer.getName());
 			if (Bukkit.getPluginManager().isPluginEnabled("SuperVanish") || Bukkit.getPluginManager().isPluginEnabled("PremiumVanish")) {
 				VanishAPI.hidePlayer(player);
+				player.sendMessage("You've been eliminated. Entering Spectator mode.");
 				}
+			else player.sendMessage("You've been eliminated. Please avoid interfering with play.");
 			new McRoyaleDeathRunnable(world,location).runTaskLater(McRoyale.getInst(), 300L);
-			Bukkit.broadcastMessage(killer.getName()+" killed "+player.getName());
 		}
-		else Bukkit.getLogger().info("not by player");
+		else Bukkit.getLogger().info(player.getName()+" killed not by player");
 
 	}
 		
