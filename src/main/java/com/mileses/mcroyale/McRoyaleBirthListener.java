6f172101d47a6ad2b289190cea69ba1977231fc0
milesnp <milesnp+github@gmail.com>
2019-03-16 17:48:28 -0700

    add respawn listen but not implement
    its broken

--- src/main/java/com/mileses/mcroyale/McRoyaleBirthListener.java
+++ src/main/java/com/mileses/mcroyale/McRoyaleBirthListener.java
@@ -1,5 +1,26 @@
 package com.mileses.mcroyale;
 
-public class McRoyaleBirthListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
+import id.cimbraien.compassradar.CompassRadar;
+
+public class McRoyaleBirthListener implements Listener{
+	
+	McRoyale pl;
+
+	public McRoyaleBirthListener(McRoyale plugin) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent e) {
+		String playername = e.getPlayer().getName();
+		if (!pl.playerlist.get(playername)) {
+			e.getPlayer().getInventory().addItem(CompassRadar.getTracker());
+		}
+	}
 }
+
